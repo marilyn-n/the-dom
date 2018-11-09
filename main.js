c8f4fca615f8a1738b737acae60b6c5ebf23cf2f
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-11-08 22:22:23 -0600

    sock styles

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const images = document.querySelectorAll('img');
 
 const toggleClass = function () {
-  this.classList.toggle('clicked');
+  this.classList.toggle('mouseover');
 };
 
-[...images].map(image => image.addEventListener('click', toggleClass));
+// const cb = function () {
+//   this.classList.toggle('mouseover');
+// };
+
+[...images].map(image => image.addEventListener('mouseover', toggleClass));
+
+// images.forEach(image => image.addEventListener('click', toggleClass));
+
+// [...images].map(img => img.addEventListener('click', cb))
